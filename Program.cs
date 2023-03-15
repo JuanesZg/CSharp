5279448d343bd59f1c643da4bf2374c3cc34999a
JuanesZg <juanesgutierrez64@gmail.com>
2023-03-15 11:46:58 -0500

    Clase con la eleccion de operaciones

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ int num_uno = int.Parse(Console.ReadLine());
 Console.WriteLine("Numero Dos");
 int num_dos = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Que operacion desea realizar?\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division");
+int opera = int.Parse(Console.ReadLine());
+
 //Se crea un objeto o instancia a partir de la clase
 var operacion_uno = new OperacionesAritmeticas(){
     numero_uno = num_uno,
-    numero_dos = num_dos
+    numero_dos = num_dos,
+    operation = opera
 };
 //Puedo tener varios objetos o instancias referidos de la misma clase
 var operacion_dos = new OperacionesAritmeticas(){
@@ -18,7 +22,5 @@ var operacion_dos = new OperacionesAritmeticas(){
 
 //Para mostrar los resultados de la suma, se debe hacer la operacion a traves
 //Del Objeto ya que es la que ahora tiene los atributos
-operacion_uno.sumar_num();
-operacion_uno.restar_num();
-operacion_uno.multiplicar_numeros();
-operacion_uno.dividir_numeros();
+
+operacion_uno.operacionRealizar();

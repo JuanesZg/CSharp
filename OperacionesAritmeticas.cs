60b39d64b82ed6146689fea61fe0fa461e91a93b
JuanesZg <juanesgutierrez64@gmail.com>
2023-03-15 21:33:35 -0500

    Metodo para escoger la operacion a realizar

--- OperacionesAritmeticas.cs
+++ OperacionesAritmeticas.cs
@@ -2,6 +2,7 @@ class OperacionesAritmeticas{
     //Propiedades - Atributos
     public int numero_uno { get; set; }
     public int numero_dos { get; set; }
+    public int operation { get; set; }
     //Metodos (Funciones o Acciones)
     public void sumar_num (){
         //Operaciones
@@ -22,6 +23,28 @@ class OperacionesAritmeticas{
     public void dividir_numeros (){
         int Resultado = numero_uno / numero_dos;
         Console.WriteLine("El resultado de la division es: " + Resultado);
+    }
+    public void operacion_num (){
+        if (operation == 1)
+        {
+            sumar_num();
+        }
+        else if (operation == 2)
+        {
+            restar_num();
+        }
+        else if (operation == 3)
+        {
+            multiplicar_numeros();
+        }
+        else if (operation == 4)
+        {
+            dividir_numeros();
+        }
+        else
+        {
+            Console.WriteLine("Operación inválida");
+        }
     }
     //Metodo constructor - no se ve pero siempre se llama igual que la clase
 }
